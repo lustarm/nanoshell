0617080996154cd135fd327971c9a7a87a2e6508
jewelrex <valve.valve101@gmail.com>
2025-02-01 23:34:18 -0500

    change some stuff

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io::{stdin, stdout, Write};
 
 struct Commands {
     name: String,
+
     // ! string for now
     output: String,
 }
@@ -10,11 +11,12 @@ impl Commands {
     fn new(i: &str) -> Self {
         Commands {
             name: i.to_string(),
-            output: "fuck u\r\n".to_string()
+            output: "test complete\r\n".to_string()
         }
     }
 
     fn spawn(self) -> std::io::Result<()> {
+
         match self.name.as_ref() {
             "hello" => stdout().write_all(self.output.as_bytes())?,
             _ => stdout().write_all(b"Invalid command\r\n")?,
@@ -26,16 +28,17 @@ impl Commands {
 }
 
 fn main() -> std::io::Result<()>{
-    stdout().write_all(b"proompt: ")?;
+    loop {
+        print!("proompt: ");
+        stdout().flush()?;
 
-    let mut i = String::new();
+        let mut i = String::new();
 
-    stdin().read_line(&mut i)?;
+        stdin().read_line(&mut i)?;
 
-    let cmd = i.trim();
+        let cmd = i.trim();
 
-    Commands::new(cmd)
-        .spawn()?;
-
-    return Ok(());
+        Commands::new(cmd)
+            .spawn()?;
+    }
 }
